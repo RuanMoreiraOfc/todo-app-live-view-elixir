d2ad564cfd2255723107adab4bb6f4cbd08e0b07
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-25 21:12:41 -0300

    feat: render template

--- lib/todo_web/live/todo_live.ex
+++ lib/todo_web/live/todo_live.ex
@@ -1,9 +1,10 @@
 defmodule TodoWeb.TodoLive do
   use Phoenix.LiveView
 
-  def render(assigns) do
-    ~H"""
-    TODO: todo app
-    """
+  import TodoWeb.TodoComponent
+  import TodoWeb.FilterComponent
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, uncompleted_count: 0)}
   end
 end
