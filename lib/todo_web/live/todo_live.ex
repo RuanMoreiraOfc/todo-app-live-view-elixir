b5b491d9c667b5e68a88241745ca01fbeab7e0b5
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-26 07:46:25 -0300

    feat: create "toggle-darkmode" event

--- lib/todo_web/live/todo_live.ex
+++ lib/todo_web/live/todo_live.ex
@@ -5,6 +5,10 @@ defmodule TodoWeb.TodoLive do
   import TodoWeb.FilterComponent
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, uncompleted_count: 0)}
+    {:ok, assign(socket, darkmode?: nil)}
+  end
+
+  def handle_event("toggle-darkmode", _params, socket) do
+    {:noreply, assign(socket, darkmode?: !socket.assigns.darkmode?)}
   end
 end
